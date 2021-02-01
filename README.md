b64ff08576c95b3b4bd253c11165f24ab200de7f
Poly <0x20fa@gmail.com>
2021-02-01 17:29:38 +0100

    Move to Visual Studio

--- README.md
+++
@@ -1,2 +0,0 @@
-<h1 align="center">voxee</h1>
-<p align="center">Learning C++ by building a space invaders clone </p>
---
+++ voxee/main.cpp
@@ -0,0 +1,10 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	std::cout << "Hello World!" << endl;
+	return 0;
+}
